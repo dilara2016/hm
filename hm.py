c09a32d317f5431feb2ed6df549f1262dcdaa427
dilara2016 <m.123.hunain@gmail.com>
2024-10-26 10:08:06 +0200

    string

---
+++ hm.py
@@ -0,0 +1,9 @@
+#enter string please
+string = input("Please enter the string for hm : ") 
+string2 = ('')
+#this is string
+for i in string:
+    string2 = i+string2
+
+print("/nThe Original String = ", string)
+print("/nThe Original String2 = ", string2)
