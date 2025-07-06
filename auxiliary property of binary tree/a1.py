ca65961c87aaac478eaf3672f588f9ce2412369b
dilara2016 <m.123.hunain@gmail.com>
2025-07-06 12:15:55 +0200

    a1

---
+++ auxiliary property of binary tree/a1.py
@@ -0,0 +1,46 @@
+class Node:
+
+    def __init__(self,data):
+        self.left_child = None
+        self.data = data
+        self.right_child = None
+
+
+def find_size_recursive(root):
+    if root is None:
+        return 0
+    return find_size_recursive(root.left_child) + find_size_recursive(root.right_child) +1
+
+def find_size_iterative(root):
+    if root is None:
+        return 0
+    count = 0
+    stack = []
+    stack.append(root)
+
+    while stack:
+        node = stack.pop()
+        count += 1
+
+        if node.left_child is not None:
+            stack.append(node.left_child)
+        if node.right_child is not None:
+            stack.append(node.right_child)
+
+    return count
+
+root = Node(1)
+root.left_child = Node(2)
+root.right_child = Node(3)
+root.left_child.left_child = Node(4)
+root.left_child.right_child = Node(5)
+root.right_child.left_child = Node(6)
+root.right_child.right_child = Node(7)
+root.right_child.right_child.left_child = Node(8)
+root.right_child.right_child.right_child = Node(7)
+
+size_recursive = find_size_recursive(root)
+print(f"the size of the binary tree <R> is: {size_recursive}")
+
+size_iterative = find_size_iterative(root)
+print(f"\nthe size of the binary tree <I> is: {size_iterative}")
